e6eecd7191b9f5aa62c5ea9dd3c44d8833a74358
Tarikul69 <tarikulabir69@gmail.com>
2024-08-15 21:15:44 +0600

    DB

--- mysite/aiquest/studymart/About_us/urls.py
+++ mysite/aiquest/studymart/About_us/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', views.about_us),
+    path('t/', views.teachers_info),
 ]
--- mysite/aiquest/studymart/About_us/views.py
+++ mysite/aiquest/studymart/About_us/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from About_us.models import Teachers
+
 # Create your views here.
 def about_us(request):
     return render(request, 'about/about.html')
+
+def teachers_info(request):
+    teach = Teachers.objects.all()
+    return render(request, 'about/t.html', {'thr': teach}) 
