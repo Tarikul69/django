ea7085c1ccff13ee6e1eb4fd6c92dc1aba22925b
Tarikul69 <tarikulabir69@gmail.com>
2024-08-16 16:50:39 +0600

    Admin Update

--- mysite/aiquest/studymart/About_us/admin.py
+++ mysite/aiquest/studymart/About_us/admin.py
@@ -3,4 +3,7 @@ from django.contrib import admin
 from About_us.models import Teachers
 
 # Register your models here.
-admin.site.register(Teachers)
+class teacher_admin(admin.ModelAdmin):
+    list_display = ('id', 'tid', 'tname', 'temail')
+
+admin.site.register(Teachers, teacher_admin)
